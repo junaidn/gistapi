26e84c6187b563e4ff5b2c8c71e6d4a7172a2e46
junaidn <junaidnaseer137@gmail.com>
2023-03-24 18:00:44 +0500

    icon button styles

---
+++ src/components/IconButton/Styles.js
@@ -0,0 +1,11 @@
+import { COLORS } from "../../utils/colors";
+
+export const iconButtonStyles = {
+  content: {
+    display: "flex",
+    gap: "4px",
+    alignItems: "center",
+    color: COLORS.blue,
+    fontWeight: "bold",
+  },
+};
